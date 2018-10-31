68df1deb3621c8ea88071271adc4d03fae495fec
Max Stevenson <maxstevenson@msn.com>
2018-10-31 15:23:46 +0000

    Challenge 11 completed
    Modified DockingStation to include a method dock_bike() that stores a bike argument as an instance variable
    
    Feature and unit tests pass as expected

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,19 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-	it {is_expected.to respond_to :release_bike}
+	it {is_expected.to respond_to :bike}
+
+	describe '#release_bike' do
+		it "creates a new instance of a Bike" do
+			object = subject.release_bike()
+			expect(object).to be_a_kind_of(Bike)
+		end
+	end
+
+	describe '#dock_bike' do
+		it "store a bike argument as an attribute of a Docking Station instance" do
+			bike = Bike.new
+			expect(subject.dock_bike(bike)).to eq bike
+		end
+	end
 end
