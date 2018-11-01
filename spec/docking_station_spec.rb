75309ca8122cf2aa0f611639ca007fb6e1018fe2
Max Stevenson <maxstevenson@msn.com>
2018-11-01 11:25:18 +0000

    General refactor of docking_station_spec
    Modified code to use let block that defines global DockingStation and Bike objects for use in tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,22 +1,24 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+	let(:station) {station = DockingStation.new}
+	let(:bike) {bike = Bike.new}
+
 	it {is_expected.to respond_to :bike}
 
 	describe '#release_bike' do
 		it "releases a Bike object" do
-			station = subject.dock_bike(Bike.new)
-			object = subject.release_bike
-			expect(object).to be_a_kind_of(Bike)
+			station.dock_bike(bike)
+			expect(station.release_bike).to be_a_kind_of(Bike)
 		end
 	end
 
-	describe 'releases working bikes'
+	describe 'releases working bikes' do
 		it 'docking station should release working bikes' do
-    	station = subject.dock_bike(Bike.new)
-    	bike = subject.release_bike
-    	expect(bike).to be_working
+			station.dock_bike(bike)
+    	expect(station.release_bike).to be_working
   	end
+  end
 
 	describe '#dock_bike' do
 		it "store a bike argument as an attribute of a Docking Station instance" do
@@ -27,14 +29,14 @@ describe DockingStation do
 
 	describe 'no bikes available' do
 		it "throws an error if release bike is called on a empty docking station" do
-			expect { DockingStation.new.release_bike }.to raise_error("No bikes available")
+			expect { station.release_bike }.to raise_error("No bikes available")
 		end
 	end
 
 	describe 'no capacity' do 
 		it "throws an error if store bike is called on a full docking station" do
-			subject.dock_bike(Bike.new)
-			expect {subject.dock_bike(Bike.new)}.to raise_error("No capacity")
+			station.dock_bike(bike)
+			expect {station.dock_bike(Bike.new)}.to raise_error("No capacity")
 		end
 	end
 end
